559a7872d0d527773de4da3f98064536ed640f5b
Moses <mosesaizee@outlook.com>
2022-01-08 18:48:37 +0000

    feat: 2nd working example

--- src/controllers/download-xlsx.ts
+++ src/controllers/download-xlsx.ts
@@ -1,20 +1,51 @@
 import { Request, Response } from "express";
-// @ts-ignore
-import * as json2xls from "json2xls";
-import * as fs from "fs";
+import * as xlsx from "json-as-xlsx";
+
+// // @ts-ignore
+// import * as json2xls from "json2xls";
+// import * as fs from "fs";
 
 export default async function downloadXlsx(req: Request, res: Response) {
   try {
-    const json = [
-      { id: 1, name: "John Doe", dob: new Date(Date.now()) },
-      { id: 2, name: "Jane Doe", dob: new Date(Date.now()) },
+    const data: any[] = [
+      {
+        sheet: "Adults",
+        columns: [
+          { label: "Name", value: "name" },
+          { label: "Age", value: "age" },
+        ],
+        content: [
+          { name: "Monserrat", age: 21 },
+          { name: "Luis", age: 22 },
+        ],
+      },
     ];
-    let fileName = "test.xlsx";
-    let xls = json2xls(json);
-    fs.writeFileSync(fileName, xls, "binary");
-    res.download(fileName, () => {
-      fs.unlinkSync(fileName);
+
+    const settings: any = {
+      writeOptions: {
+        type: "buffer",
+        bookType: "xlsx",
+      },
+    };
+
+    // @ts-ignore
+    const buffer = xlsx(data, settings);
+    res.writeHead(200, {
+      "Content-Type": "application/octet-stream",
+      "Content-disposition": "attachment; filename=MySheet.xlsx",
     });
+    res.end(buffer);
+
+    // const json = [
+    //   { id: 1, name: "John Doe", dob: new Date(Date.now()) },
+    //   { id: 2, name: "Jane Doe", dob: new Date(Date.now()) },
+    // ];
+    // let fileName = "test.xlsx";
+    // let xls = json2xls(json);
+    // fs.writeFileSync(fileName, xls, "binary");
+    // res.download(fileName, () => {
+    //   fs.unlinkSync(fileName);
+    // });
   } catch (e) {
     res.status(500).json({ error: "Server error" });
   }
