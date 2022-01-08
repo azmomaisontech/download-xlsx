cb00e3a5f61f046800ae81ae5152ec0a59c39541
Moses <mosesaizee@outlook.com>
2022-01-08 18:34:18 +0000

    feat: first working example

--- src/controllers/download-xlsx.ts
+++ src/controllers/download-xlsx.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from "express";
+// @ts-ignore
+import * as json2xls from "json2xls";
+import * as fs from "fs";
 
-export default function downloadXlsx(req: Request, res: Response) {
+export default async function downloadXlsx(req: Request, res: Response) {
   try {
-    res.status(200).json({ data: "Working" });
+    const json = [
+      { id: 1, name: "John Doe", dob: new Date(Date.now()) },
+      { id: 2, name: "Jane Doe", dob: new Date(Date.now()) },
+    ];
+    let fileName = "test.xlsx";
+    let xls = json2xls(json);
+    fs.writeFileSync(fileName, xls, "binary");
+    res.download(fileName, () => {
+      fs.unlinkSync(fileName);
+    });
   } catch (e) {
     res.status(500).json({ error: "Server error" });
   }
